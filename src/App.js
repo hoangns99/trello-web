e1e572a15968dcad958ad2b1efeae47e0a516fb2
nsh <nshoang99.dev@gmail.com>
2022-01-09 12:38:56 +0700

    Clean React Architecture

--- src/App.js
+++ src/App.js
@@ -1,171 +1,17 @@
 
+import React from 'react';
 import './App.scss';
+import AppBar from 'components/AppBar/AppBar'
+import BoardBar from 'components/BoardBar/BoardBar'
+import BoardContent from 'components/BoardContent/BoardContent'
 
 function App() {
   return (
     <div className="trello-hoangns-master">
-      <nav className="navbar app"> App Bar</nav>
-      <nav className="navbar board"> Board Bar</nav>
-      <div className="board-columns">
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/217398019_1209586872846829_8062224018798359500_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=XBIv7I9QtUcAX9OCe6r&_nc_ht=scontent.fdad1-3.fna&oh=00_AT9yCZ2i6uELuY-uqHj3zlmz12g4KArNUtzXsivtHHp25A&oe=61DDF7C7" alt="" />
-              Title: NGUYỄN SONG HOÀNG
-            </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
 
-
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/217398019_1209586872846829_8062224018798359500_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=XBIv7I9QtUcAX9OCe6r&_nc_ht=scontent.fdad1-3.fna&oh=00_AT9yCZ2i6uELuY-uqHj3zlmz12g4KArNUtzXsivtHHp25A&oe=61DDF7C7" alt="" />
-              Title: NGUYỄN SONG HOÀNG
-            </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-
-
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/217398019_1209586872846829_8062224018798359500_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=XBIv7I9QtUcAX9OCe6r&_nc_ht=scontent.fdad1-3.fna&oh=00_AT9yCZ2i6uELuY-uqHj3zlmz12g4KArNUtzXsivtHHp25A&oe=61DDF7C7" alt="" />
-              Title: NGUYỄN SONG HOÀNG
-            </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/217398019_1209586872846829_8062224018798359500_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=XBIv7I9QtUcAX9OCe6r&_nc_ht=scontent.fdad1-3.fna&oh=00_AT9yCZ2i6uELuY-uqHj3zlmz12g4KArNUtzXsivtHHp25A&oe=61DDF7C7" alt="" />
-              Title: NGUYỄN SONG HOÀNG
-            </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/217398019_1209586872846829_8062224018798359500_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=XBIv7I9QtUcAX9OCe6r&_nc_ht=scontent.fdad1-3.fna&oh=00_AT9yCZ2i6uELuY-uqHj3zlmz12g4KArNUtzXsivtHHp25A&oe=61DDF7C7" alt="" />
-              Title: NGUYỄN SONG HOÀNG
-            </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className='column'>
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/217398019_1209586872846829_8062224018798359500_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=XBIv7I9QtUcAX9OCe6r&_nc_ht=scontent.fdad1-3.fna&oh=00_AT9yCZ2i6uELuY-uqHj3zlmz12g4KArNUtzXsivtHHp25A&oe=61DDF7C7" alt="" />
-              Title: NGUYỄN SONG HOÀNG
-            </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-            <li> Add what you'd like to work on below </li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
     </div>
   );
 }
